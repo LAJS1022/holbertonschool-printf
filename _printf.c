93367384167fddc801636bb52bb6bedbfac1506e
LuisJimenez <luisssajs@gmail.com>
2025-11-20 16:33:01 +0000

    Task 1

--- _printf.c
+++ _printf.c
@@ -20,6 +20,12 @@ int _printf(const char *format, ...)
 
     while (format[i])
     {
+        if (format[i] == '%' && format[i + 1] == '\0')
+        {
+            va_end(args);
+            return (-1);
+        }
+
         if (format[i] == '%')
         {
             i++;
